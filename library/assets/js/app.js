1c2ff8feb10b70e22ffa7f9378495ac7b89eeef3
sauliusgto3000 <sauliusgto3000@gmail.com>
2018-09-17 18:20:39 +0300

    Bootstrap 4.1.3 added

--- library/assets/js/app.js
+++ library/assets/js/app.js
@@ -1,3 +1,16 @@
 require('../css/app.scss');
 
 console.log('Hello Webpack Encore');
+
+const $ = require('jquery');
+// JS is equivalent to the normal "bootstrap" package
+// no need to set this to a variable, just require it
+require('bootstrap');
+
+// or you can include specific pieces
+// require('bootstrap/js/dist/tooltip');
+// require('bootstrap/js/dist/popover');
+
+$(document).ready(function() {
+    $('[data-toggle="popover"]').popover();
+});
